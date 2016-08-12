1aedddc3ef20800ff3363edf654d6f5dc4053d35
legodude17 <legodudejb@gmail.com>
2016-08-12 08:19:40 -0700

    Now the whole "task manager" thing works.

--- cli.js
+++ cli.js
@@ -1,3 +1,55 @@
 #! /usr/bin/node
 "use strict";
-var quake = require('.');
+const create = require('.');
+const program = require('commander');
+const chalk = require('chalk');
+const path = require('path');
+const logSymbols = require('log-symbols');
+const fs = require('fs');
+const filesToLookFor = ['Quakefile.js', 'quakefile.js', 'Taskfile.js', 'taskfile.js']
+program
+  .version(require('./package.json').version)
+  .usage('[task] [options]')
+  .option('--file <file>', 'Use <file> for task running.')
+  .option('-f, --force', 'Force continuation after error.');
+program.parse(process.argv);
+function findFile() {
+  if (program.file) {
+    var file = path.join(process.cwd(), program.file);
+    if (fs.existsSync(file)) {
+      return {result: file, func: require(file)};
+    }
+    exit('Could not find file ' + chalk.red(file));
+  } else {
+    var res = filesToLookFor.map(function (v) {
+      return path.join(process.cwd(), v);
+    }).map(fs.existsSync).reduce(function (res, v, i) {
+      if (res) {
+        return res;
+      }
+      if (v) {
+        return filesToLookFor[i];
+      }
+      return false;
+    }, false);
+    if (res) {
+      return {result: res, func: require(res)};
+    }
+    exit('Could not find task file. Specify with ' + chalk.yellow('--file') + ' or name it one of ' + chalk.yellow(filesToLookFor.slice(0, filesToLookFor.length - 1).concat('or ' + filesToLookFor[filesToLookFor.length - 1]).join(', ')));
+  }
+}
+var {func, result} = findFile();
+if (typeof func !== 'function') {
+  exit('Export of task file ' + chalk.yellow(result) + ' is not a function.');
+}
+var quake = create();
+func(quake.passer);
+quake._start(program.task || 'default');
+function exit(message) {
+  if (message) {
+    console.error(logSymbols.error + '  ' + message);
+    process.exit(1);
+    return;
+  }
+  process.exit(0);
+}
--- test.js
+++ test.js
@@ -1,30 +1,27 @@
 "use strict";
-const Quake = require('./lib/Quake');
-var quake = new Quake();
-quake.add('Buy ingredients', function (cb) {
-  setTimeout(function () {
-    quake.add('Profit', ['Eat cake'], function (cb) {
-      setTimeout(function () {
-        cb(new Error('Nope'));
-      }, 1000);
-    });
-    quake.start('Profit');
-    cb();
-  }, 1000);
-});
-quake.add('Make cake', ['Buy ingredients'], function (cb) {
-  setTimeout(cb, 2000);
-});
-quake.add('Eat cake', ['Make cake'], function (cb) {
-  setTimeout(cb, 3000);
-});
-quake.add('Make lemonade', ['Buy ingredients'], function (cb) {
-  setTimeout(cb, 500);
-});
-quake.add('Drink lemonade', ['Make lemonade'], function (cb) {
-  setTimeout(cb, 4000);
-});
-quake.add('Be happy', ['Eat cake', 'Drink lemonade'], function (cb) {
-  setTimeout(cb, 1234);
-});
-quake.start('Be happy');
+module.exports = function (quake) {
+  quake.add('Buy ingredients', function (cb) {
+    setTimeout(cb, 1000);
+  });
+  quake.add('Make cake', ['Buy ingredients'], function (cb) {
+    setTimeout(cb, 2000);
+  });
+  quake.add('Eat cake', ['Make cake'], function (cb) {
+    setTimeout(cb, 3000);
+  });
+  quake.add('Make lemonade', ['Buy ingredients'], function (cb) {
+    setTimeout(cb, 500);
+  });
+  quake.add('Drink lemonade', ['Make lemonade'], function (cb) {
+    setTimeout(cb, 4000);
+  });
+  quake.add('Be happy', ['Eat cake', 'Drink lemonade'], function (cb) {
+    setTimeout(cb, 1234);
+  });
+  quake.add('Profit', ['Eat cake'], function (cb) {
+    setTimeout(function () {
+      cb(new Error('Error!'));
+    }, 4000);
+  });
+  quake.add('default', ['Be happy', 'Profit']);
+}
